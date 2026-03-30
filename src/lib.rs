274862abb304a92fd1cad1e11b70230891f4531e
TBA5854 <adhavan5030@gmail.com>
2026-03-31 02:46:56 +0530

    feat: export new AQP and ingestion modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod aqp;
 pub mod config;
 pub mod datagen;
+pub mod driver;
 pub mod errors;
+pub mod ingestion;
 pub mod query;
 pub mod repl;
 pub mod storage;
@@ -11,4 +13,5 @@ pub mod utils;
 pub use config::*;
 pub use datagen::*;
 pub use errors::*;
+pub use ingestion::Connection;
 pub use types::*;
